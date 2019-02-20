aa9c99c7e81fe31e8196537f396660712058e0c3
Luiz tanure <letanure@gmail.com>
2019-02-20 11:03:28 +0100

    feat: add language currency and couintry selection to config menu

--- src/menuConfig.js
+++ src/menuConfig.js
@@ -3,9 +3,15 @@
 
 const inquirer = require('inquirer')
 const clear = require('clear')
+const CliTable = require('cli-table2')
+const languagesMenu = require('./languages')
+const currenciesMenu = require('./currencies')
+const countriesMenu = require('./countries')
+const dataStorage = require('./helpers/dataStorage')
 
 const menuConfig = async () => {
   clear()
+  tableConfig()
   return inquirer
     .prompt([
       {
@@ -23,15 +29,27 @@ const menuConfig = async () => {
     ])
 }
 
+const tableConfig = () => {
+  const table = new CliTable()
+  const data = dataStorage.getData()
+  Object.keys(data.config).map(keyConfig => {
+    table.push({ [keyConfig]: data.config[keyConfig] })
+  })
+  console.log(table.toString())
+}
+
 const editConfig = async () => {
   const configToEdit = await menuConfig()
   let valueUpdated = null
   switch (configToEdit.value) {
     case 'currency':
+      valueUpdated = await currenciesMenu.currencySelect('Select currency:')
       break
     case 'country':
+      valueUpdated = await countriesMenu.countrySelect('Select country:')
       break
     case 'language':
+      valueUpdated = await languagesMenu.languageSelect('Select language:')
       break
     case 'back':
       const index = require('./index.js')
@@ -39,6 +57,7 @@ const editConfig = async () => {
       break
   }
   if (valueUpdated) {
+    dataStorage.saveConfig(configToEdit.value, valueUpdated.value)
     editConfig()
   }
 }
