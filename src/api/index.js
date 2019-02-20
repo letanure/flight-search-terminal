315b35c5c93f56b7e60a2ee1ad1a94fed63ddbe1
Luiz tanure <letanure@gmail.com>
2019-02-20 10:52:42 +0100

    feat: add list places api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-axios.create({
+const instance = axios.create({
   baseURL: 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/',
   timeout: 2000,
   headers: {
@@ -8,5 +8,12 @@ axios.create({
   }
 })
 
+const listPlaces = async (query, countryCode, currencyCode = 'EUR', localeISO = 'en-GB') => {
+  const response = await instance.get(`autosuggest/v1.0/${countryCode}/${currencyCode}/${localeISO}/?query=${query}`)
+  const data = await response.data.Places
+  return data
+}
+
 module.exports = {
+  listPlaces
 }
